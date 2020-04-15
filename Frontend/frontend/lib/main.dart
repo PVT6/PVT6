93557e71317f9da822c00a737260797be2c5edde
jakobodman1997 <63099191+jakobodman1997@users.noreply.github.com>
2020-04-15 16:58:08 +0200

    Google API i flutter

--- Frontend/frontend/lib/main.dart
+++ Frontend/frontend/lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -199,7 +201,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => MainPage()),
+                      MaterialPageRoute(builder: (context) => MapsDemo()),
                     );
                   },
                   child: new Text("Continue without login >",
@@ -269,7 +271,7 @@ class SecondRoute extends StatelessWidget {
         onPressed: () {
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => MainPage()),
+            MaterialPageRoute(builder: (context) => MapsDemo()),
           );
         },
         child: Text("Login",
@@ -352,56 +354,182 @@ class SecondRoute extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
-  final TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+class MapsDemo extends StatefulWidget {
+  MapsDemo() : super();
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-  //       child: Container(
-  // decoration: BoxDecoration(
-  //   image: DecorationImage(
-  //     image: AssetImage("assets/mapPlaceholder.png"),
-  //     fit: BoxFit.cover,
-  //   ),
-  // ),
-  //         //child: BottomAppBar()
-//         ),
-//       ),
-//     );
-//   }
-// }
+  final String title = "Maps Demo";
 
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(title: const Text('Tasks - Bottom App Bar')),
-      floatingActionButton: FloatingActionButton.extended(
-        elevation: 4.0,
-        icon: const Icon(Icons.add),
-        label: const Text('New route'),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => MyHomePage()),
-          );
-        },
+  @override
+  MapsDemoState createState() => MapsDemoState();
+}
+
+class MapsDemoState extends State<MapsDemo> {
+  //
+  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng _center = const LatLng(59.334591, 18.063240);
+  final Set<Marker> _markers = {};
+  LatLng _lastMapPosition = _center;
+  MapType _currentMapType = MapType.normal;
+
+  static final CameraPosition _position1 = CameraPosition(
+    bearing: 192.833,
+    target: LatLng(59.334591, 18.063240),
+    tilt: 59.440,
+    zoom: 11.0,
+  );
+
+  Future<void> _goToPosition1() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_position1));
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  _onCameraMove(CameraPosition position) {
+    _lastMapPosition = position.target;
+  }
+
+  _onMapTypeButtonPressed() {
+    setState(() {
+      _currentMapType = _currentMapType == MapType.normal
+          ? MapType.satellite
+          : MapType.normal;
+    });
+  }
+
+  _onAddMarkerButtonPressed() {
+    setState(() {
+      _markers.add(
+        Marker(
+          markerId: MarkerId(_lastMapPosition.toString()),
+          position: _lastMapPosition,
+          infoWindow: InfoWindow(
+            title: 'This is a Title',
+            snippet: 'This is a snippet',
+          ),
+          icon: BitmapDescriptor.defaultMarker,
+        ),
+      );
+    });
+  }
+
+  String _randomString(int length) {
+   var rand = new Random();
+   var codeUnits = new List.generate(
+      length, 
+      (index){
+         return rand.nextInt(33)+89;
+      }
+   );
+
+   return new String.fromCharCodes(codeUnits);
+}
+
+  Widget button(Function function, IconData icon) {
+    return FloatingActionButton(
+      heroTag: _randomString(10),
+      onPressed: function,
+      materialTapTargetSize: MaterialTapTargetSize.padded,
+      backgroundColor: Colors.blue,
+      child: Icon(
+        icon,
+        size: 36.0,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        child: new Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 4.0,
+          icon: const Icon(Icons.add),
+          label: const Text('Create new route'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => MyHomePage()),
+            );
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+        body: Stack(
           children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _center,
+                zoom: 11.0,
+              ),
+              mapType: _currentMapType,
+              markers: _markers,
+              onCameraMove: _onCameraMove,
             ),
-            IconButton(
-              icon: Icon(Icons.search),
-              onPressed: () {},
-            )
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Align(
+                alignment: Alignment.topRight,
+                child: Column(
+                  children: <Widget>[
+                    button(_onMapTypeButtonPressed, Icons.map),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    button(_onAddMarkerButtonPressed, Icons.add_location),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    button(_goToPosition1, Icons.location_searching),
+                  ],
+                ),
+              ),
+            ),
           ],
+        ),
+        appBar: AppBar(title: Text("SafeLight Stockholm")),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("Jakob Ödman"),
+                accountEmail: Text("fakeMail123@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor:
+                      Theme.of(context).platform == TargetPlatform.iOS
+                          ? Colors.blue
+                          : Colors.white,
+                  child: Text(
+                    "PH", //placeholder, kanske användarbild här?
+                    style: TextStyle(fontSize: 40.0),
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  // Update the state of the app
+                  // ...
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
