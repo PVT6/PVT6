91110b4919c6203d87155a4c27bc43975db306dc
Viktor Johansson <viggego@me.com>
2020-04-20 20:25:26 +0200

    start på email verifikation

---
+++ Frontend/frontend/lib/verifyEmail.dart
@@ -0,0 +1,209 @@
+import 'dart:async';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/mapsDemo.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+Color darkGreen = Colors.green[900];
+Color lightGreen = Colors.green[100];
+
+class PinCodeVerificationScreen extends StatefulWidget {
+  PinCodeVerificationScreen();
+  @override
+  _PinCodeVerificationScreenState createState() =>
+      _PinCodeVerificationScreenState();
+}
+
+class _PinCodeVerificationScreenState extends State<PinCodeVerificationScreen> {
+  var onTapRecognizer;
+
+  TextEditingController textEditingController = TextEditingController()
+    ..text = "123456";
+
+  StreamController<ErrorAnimationType> errorController;
+
+  bool hasError = false;
+  String currentText = "";
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  @override
+  void initState() {
+    onTapRecognizer = TapGestureRecognizer()
+      ..onTap = () {
+        Navigator.pop(context);
+      };
+    errorController = StreamController<ErrorAnimationType>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    errorController.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lightGreen,
+      key: scaffoldKey,
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(FocusNode());
+        },
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: ListView(
+            children: <Widget>[
+              SizedBox(height: 30),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: Image.asset(
+                  ('assets/logophase4png.png'),
+                ),
+              ),
+              SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  'Email Verification',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30.0, vertical: 8),
+                child: RichText(
+                  text: TextSpan(
+                      text: "Enter the code sent to ",
+                      children: [
+                        TextSpan(
+                            text: '+46701234567', //Phone number from user
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15)),
+                      ],
+                      style: TextStyle(color: Colors.black54, fontSize: 15)),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30),
+                  child: PinCodeTextField(
+                    length: 6,
+                    obsecureText: false,
+                    animationType: AnimationType.fade,
+                    shape: PinCodeFieldShape.box,
+                    animationDuration: Duration(milliseconds: 300),
+                    borderRadius: BorderRadius.circular(5),
+                    fieldHeight: 50,
+                    backgroundColor: lightGreen,
+                    fieldWidth: 40,
+                    activeFillColor: Colors.white,
+                    enableActiveFill: true,
+                    errorAnimationController: errorController,
+                    controller: textEditingController,
+                    onCompleted: (v) {
+                      print("Completed");
+                    },
+                    onChanged: (value) {
+                      print(value);
+                      setState(() {
+                        currentText = value;
+                      });
+                    },
+                  )),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text(
+                  hasError ? "*Please fill up all the cells properly" : "",
+                  style: TextStyle(color: Colors.red.shade300, fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                    text: "Didn't receive the code? ",
+                    style: TextStyle(color: Colors.black54, fontSize: 15),
+                    children: [
+                      TextSpan(
+                          text: " RESEND",
+                          recognizer: onTapRecognizer,
+                          style: TextStyle(
+                              color: Color(0xFF91D3B3),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16))
+                    ]),
+              ),
+              SizedBox(
+                height: 14,
+              ),
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 30),
+                child: ButtonTheme(
+                  height: 50,
+                  child: FlatButton(
+                    //if statement, är det rätt ifyllt blir det verifierat
+                    onPressed: () {
+
+                      // conditions for validating
+                      if (currentText.length != 6 || currentText != "towtow") {
+                        errorController.add(ErrorAnimationType
+                            .shake); // Triggering error shake animation
+                        setState(() {
+                          hasError = true;
+                        });
+                      } else {
+                        setState(() {
+                          hasError = false;
+                          scaffoldKey.currentState.showSnackBar(SnackBar(
+                            content: Text("Aye!!"),
+                            duration: Duration(seconds: 2),
+                          ));
+                        });
+                      }
+                    },
+                    child: Center(
+                        child: Text(
+                      "VERIFY".toUpperCase(),
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    )),
+                  ),
+                ),
+                decoration: BoxDecoration(
+                    color: darkGreen,
+                    borderRadius: BorderRadius.circular(5),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.green.shade200,
+                          offset: Offset(1, -2),
+                          blurRadius: 5),
+                      BoxShadow(
+                          color: Colors.green.shade200,
+                          offset: Offset(-1, 2),
+                          blurRadius: 5)
+                    ]),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
