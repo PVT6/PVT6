1f2c430ec7f283edb4e3d97c7f891905273ba81b
Goblerino <teemoubastard@gmail.com>
2020-04-21 14:47:49 +0200

    services map och auth fil

---
+++ Frontend/frontend/lib/services/auth.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+// sign in with email & password
+
+// register with email & password
+
+// sign out
+
+//More sign in methods. 
+
+
+}
---
+++ Frontend/frontend/lib/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return home scrren eller authenticate widget.
+    //https://youtu.be/z05m8nlPRxk?t=394  
+    
+  }
+}
