8c12df37b7fb70a7f5891f75eb2292d3294b64b3
Goblerino <teemoubastard@gmail.com>
2020-04-21 15:13:45 +0200

    Början på lite skelett

--- Frontend/frontend/lib/screens/authenticate/loginpage.dart
+++ Frontend/frontend/lib/screens/authenticate/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/services/auth.dart';
 
 class SignIn extends StatefulWidget {
   @override
@@ -6,6 +7,9 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
