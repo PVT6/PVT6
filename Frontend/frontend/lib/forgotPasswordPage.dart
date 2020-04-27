9ca84c3951c48394023105e633592826f2bb73fa
Rasmus Hagelsten <teemoubastard@gmail.com>
2020-04-27 15:41:32 +0200

    Added validator to Forgot Password

--- Frontend/frontend/lib/forgotPasswordPage.dart
+++ Frontend/frontend/lib/forgotPasswordPage.dart
@@ -17,6 +17,7 @@ class _ForgotPasswordPage extends State<ForgotPasswordPage> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final AuthService _auth = AuthService();
   bool hasError = false;
+  final _formKey = GlobalKey<FormState>();
   String email = "";
 
 Widget build(BuildContext context) {
@@ -30,22 +31,22 @@ Widget build(BuildContext context) {
     body: Container(
       padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 50.0 ),
       child: Form(
+        key: _formKey,
         child: Column(
           children: <Widget>[
             SizedBox(height: 20.0),
             TextFormField(
+              validator: (val) => val.isEmpty ? 'Enter an email.' : null,
               onChanged: (val) {
                 setState(() => email = val);
-                print(email);
               }
             ),
             SizedBox(height: 20.0),
             RaisedButton(
               child: Text('Reset Password'),
               onPressed: () async{
-                print("Pressed button.");
-                print(email);
-                await _auth.sendPasswordResetEmail(email);
+                if(_formKey.currentState.validate()){await _auth.sendPasswordResetEmail(email);}
+                
               }
             )
           ]
