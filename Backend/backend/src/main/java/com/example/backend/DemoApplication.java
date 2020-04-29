6040c7eebd65cd28acbdb63933ecba60a7757cef
lucas-domeij <lucas@domeij.com>
2020-04-29 11:14:58 +0200

    start dog controller

--- Backend/backend/src/main/java/com/example/backend/DemoApplication.java
+++ Backend/backend/src/main/java/com/example/backend/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+
 public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
