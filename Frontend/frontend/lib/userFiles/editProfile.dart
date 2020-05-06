3a74a53345e13fababcfbfca0b8ca8aa29ccba3b
lucas-domeij <lucas@domeij.com>
2020-05-06 09:54:01 +0200

    working settings

--- Frontend/frontend/lib/userFiles/editProfile.dart
+++ Frontend/frontend/lib/userFiles/editProfile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'user.dart' as userlib;
+import 'package:http/http.dart' as http;
+
 
 class EditProfile extends StatefulWidget {
   EditProfile() : super();
@@ -16,14 +18,31 @@ class EditProfileState extends State<EditProfile> {
 
   @override
   Widget build(BuildContext context) {
-   
+   TextEditingController name = new TextEditingController(text: userlib.name);
+   TextEditingController email = new TextEditingController(text: userlib.email);
+   TextEditingController phone = new TextEditingController(text: userlib.phone);
     return new Scaffold(
       appBar: new AppBar(title: const Text('Edit Profile'), actions: <Widget>[
         new Container(
             padding: const EdgeInsets.fromLTRB(0.0, 10.0, 5.0, 10.0),
             child: new MaterialButton(
               child: new Text('Save'),
-              onPressed: () {
+              onPressed: () async {
+                var url =
+                        'https://group6-15.pvt.dsv.su.se/user/update';
+                    var response = await http.post(Uri.parse(url), body: {
+                      'name': name.text,
+                      'email': email.text,
+                      'phone': phone.text,
+                      'uid': userlib.uid
+                    });
+                    if (response.statusCode == 200) {
+                      Navigator.of(context).pop();
+                      
+                    } else {
+                      // ERROR MEDELANDE HÄR
+                    }
+                
                 //spara ändringar, kolla ner vid contacts hur man gör
               },
             ))
@@ -40,26 +59,28 @@ class EditProfileState extends State<EditProfile> {
             children: <Widget>[
               new Container(
                 child: new TextField(
-                  controller: new TextEditingController(text: userlib.name),
+                  controller: name,
                   decoration: const InputDecoration(labelText: "First Name"),
+               
                 ),
               ),
               new Container(
                 child: new TextField(
-                  controller: new TextEditingController(text: userlib.name),
+                  controller: name ,
                   decoration: const InputDecoration(labelText: "Last Name", ),
+                
                 ),
               ),
               new Container(
                 child: new TextField(
-                  controller: new TextEditingController(text: userlib.email),
+                  controller: email ,
                   decoration: const InputDecoration(
                       labelText: "Email", ), //userData // Måste vara en const så går inte
                 ),
               ),
               new Container(
                 child: new TextField(
-                  controller: new TextEditingController(text: userlib.phone),
+                  controller: phone,
                   decoration: const InputDecoration(
                       labelText: "Phonenumber", ), //userData  // Måste vara en const så går inte
                 ),
