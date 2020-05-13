bbe09695d8b8cd86bb2a0fce31d34a1dc9cdd5d1
Thomas Luostarinen <thomas@Thomass-MacBook-Pro.local>
2020-05-13 11:52:27 +0200

    Added a test into DogTest

--- Backend/backend/src/test/DogTest.java
+++ Backend/backend/src/test/DogTest.java
@@ -1,4 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class DogTest {
+    @Test
+    public void makeDogEmptyConstructor() {
+        Dog dog = new Dog();
 
+        assertNotNull(dog.getId());
+
+        assertNull(dog.getName());
+        assertNull(dog.getRace());
+        assertNull(dog.getWeight());
+        assertNull(dog.getAge());
+    }
 }
