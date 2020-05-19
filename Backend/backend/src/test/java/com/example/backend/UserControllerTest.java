04fcb16b2975a410d4fef49f5a84500bf1fb512d
Thomas Luostarinen <thomas@Thomass-MacBook-Pro.local>
2020-05-19 15:12:19 +0200

    Added 11 tests to UserControllerTest

--- Backend/backend/src/test/java/com/example/backend/UserControllerTest.java
+++ Backend/backend/src/test/java/com/example/backend/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.backend;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Set;
@@ -52,5 +53,102 @@ public class UserControllerTest {
 
         Set<Dog> dogs = usrcon.findDogs("uid");
         assertTrue(dogs.contains(dog));
+    }
+
+    @Test
+    public void findUser() {
+        UserController usrcon = new UserController();
+        User user = new User("uid", "email", "phone", "name");
+        
+        usrcon.addNewUser("uid", "email", "phone", "name");
+
+        User usrconUser = usrcon.findUser("uid");
+
+        assertEquals(user, usrconUser);
+    }
+
+    @Test
+    public void updateFail() {
+        UserController usrcon = new UserController();
+        String status = usrcon.update("uid", "name", "email", "phone");
+
+        assertEquals("no user found", status);
+    }
+
+    @Test
+    public void update() {
+        UserController usrcon = new UserController();
+        usrcon.addNewUser("uid", "email", "phone", "name");
+        String status = usrcon.update("uid", "name", "email", "phone");
+
+        assertEquals("Updated", status);
+    }
+
+    @Test
+    public void newNameForUserFail() {
+        UserController usrcon = new UserController();
+        boolean status = usrcon.newNameForUser("uid", "newName");
+
+        assertFalse(status);
+    }
+
+    @Test
+    public void newNameForUser() {
+        UserController usrcon = new UserController();
+        usrcon.addNewUser("uid", "email", "phone", "name");
+        boolean status = usrcon.newNameForUser("uid", "newName");
+
+        assertTrue(status);
+    }
+
+    @Test
+    public void newPhoneNumberForUserFail(){
+        UserController usrcon = new UserController();
+        boolean status = usrcon.newPhoneNumberForUser("uid", "newPhone");
+
+        assertFalse(status);
+    }
+
+    @Test
+    public void newPhoneNumberForUser(){
+        UserController usrcon = new UserController();
+        usrcon.addNewUser("uid", "email", "phone", "name");
+        boolean status = usrcon.newPhoneNumberForUser("uid", "newPhone");
+
+        assertTrue(status);
+    }
+
+    @Test
+    public void newEmailForUserFail() {
+        UserController usrcon = new UserController();
+        boolean status = usrcon.newEmailForUser("uid", "newEmail");
+
+        assertFalse(status);
+    }
+
+    @Test
+    public void newEmailForUser() {
+        UserController usrcon = new UserController();
+        usrcon.addNewUser("uid", "email", "phone", "name");
+        boolean status = usrcon.newEmailForUser("uid", "newEmail");
+
+        assertTrue(status);
+    }
+
+    @Test
+    public void deleteUserFail() {
+        UserController usrcon = new UserController();
+        boolean status = usrcon.deleteUser("uid");
+
+        assertFalse(status);
+    }
+
+    @Test
+    public void deleteUser() {
+        UserController usrcon = new UserController();
+        usrcon.addNewUser("uid", "email", "phone", "name");
+        boolean status = usrcon.deleteUser("uid");
+
+        assertTrue(status);
     }
 }
