6ed316502e6abc86184553dffd1ad99f440864c4
Thomas Luostarinen <thomas@Thomass-MacBook-Pro.local>
2020-05-20 14:25:56 +0200

    Created 2 files, ContactListTest with 4 tests, DogControllerTest with 1 comment

---
+++ Backend/backend/src/test/java/com/example/backend/ContactListTest.java
@@ -0,0 +1,49 @@
+package com.example.backend;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+public class ContactListTest {
+
+    @Test
+    public void getIdNoSet() {
+        ContactList cl = new ContactList();
+        long id = cl.getId();
+
+        assertNotNull(id);
+    }
+
+    @Test
+    public void getIdAfterSet() {
+        ContactList cl = new ContactList();
+        long setId = 1;
+        cl.setId(setId);
+        long getId = cl.getId();
+
+        assertEquals(setId, getId);
+    }
+
+    @Test
+    public void getUserNoSet() {
+        ContactList cl = new ContactList();
+        Set<User> users = cl.getUser();
+        
+        assertTrue(users.isEmpty());
+    }
+
+    @Test
+    public void getUserAfterSet() {
+        ContactList cl = new ContactList();
+        User user = new User("uid");
+        cl.setUser(user);
+        Set<User> users = cl.getUser();
+
+        assertFalse(users.isEmpty());
+    }
+}
