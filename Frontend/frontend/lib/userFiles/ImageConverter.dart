052a86ef3b478d7ba6c68c15f29e2461f530ecb9
Martin Lundberg <martinlundber@gmail.com>
2020-05-28 16:32:00 +0200

    la till ImageConverter metoder

--- Frontend/frontend/lib/userFiles/ImageConverter.dart
+++ Frontend/frontend/lib/userFiles/ImageConverter.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io' as Io;
 import 'dart:typed_data';
 import 'package:flutter/widgets.dart';
 
@@ -8,8 +9,10 @@ Image imageFromBase64String(String base64String) {
   return Image.memory(base64Decode(base64String));
 }
 
-String base64StringFromImage(File image){
-  
+String base64StringFromImage(Image image){
+  final bytes = Io.File('image').readAsBytesSync();
+  String base64String = base64Encode(bytes);
+  return base64String;
 }
 
 }
