c3857dff526806aee9ee5a1e272712f3861073d4
Martin Lundberg <martinlundber@gmail.com>
2020-05-28 11:35:43 +0200

    imageConverter

--- Frontend/frontend/lib/userFiles/ImageConverter.dart
+++ Frontend/frontend/lib/userFiles/ImageConverter.dart
@@ -2,8 +2,14 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'package:flutter/widgets.dart';
 
-class ImageConverter extends StatelessWidget {
-  ImageConverter(): super();
+class ImageConverter {
 
+Image imageFromBase64String(String base64String) {
+  return Image.memory(base64Decode(base64String));
+}
+
+String base64StringFromImage(File image){
+  
+}
 
 }
